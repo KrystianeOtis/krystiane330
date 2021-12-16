7e880ba1e4d563282cbccca2c144e1bc97e68902
KrystianeOtis <oti19001@byui.edu>
2021-12-15 20:12:41 -0700

    overlay!!!!

--- FinalProject/public/js/script.js
+++ FinalProject/public/js/script.js
@@ -1,5 +1,3 @@
-
-
 var request = new XMLHttpRequest()
 request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
 request.onload = function () {
@@ -24,25 +22,66 @@ request.onload = function () {
             img.setAttribute('id', movie.id)
             img.onclick = getID
 
-            const h1 = document.createElement('h1')
-            h1.textContent = movie.title
-            h1.setAttribute('class', 'movie-info')
 
-            const h2 = document.createElement('h2')
-            h2.textContent = movie.original_title
-            h2.setAttribute('class', 'movie-info')
-
-            const p = document.createElement('p')
-            movie.description = movie.description.substring(0, 300)
-            p.textContent = `${movie.description}...`
-            h2.setAttribute('class', 'overview')
 
             container.appendChild(card)
             card.appendChild(img)
-            // card.appendChild(h1)
-            // card.appendChild(h2)
-            // card.appendChild(p)
+
+
+
+
+
+
         })
+
+
+        document.querySelectorAll('.movie-poster').forEach(item => {
+            item.addEventListener('click', e => {
+                let x = e.target.getAttribute("id");
+                console.log(x)
+
+
+                async function getMovie() {
+                    let movieUrl = `https://ghibliapi.herokuapp.com/films/${x}`;
+
+                    let movie = [];
+
+                    const response = await fetch(movieUrl);
+
+                    const responseData = await response.json();
+
+                    movie = responseData;
+
+                    console.log(movie.title)
+
+                    let modul = document.getElementById("modul");
+                    modul.style.display = 'block';
+
+                    let modulmain = document.getElementById("modulmain");
+
+                    const h1 = document.createElement('h1')
+                    h1.innerHTML = movie.title
+                    h1.setAttribute('class', 'movie-info')
+
+                    const h2 = document.createElement('h2')
+                    h2.innerHTML = movie.original_title
+                    h2.setAttribute('class', 'movie-info')
+
+                    const p = document.createElement('p')
+                    movie.description = movie.description.substring(0, 300)
+                    p.innerHTML = `${movie.description}...`
+                    p.setAttribute('class', 'movie-info')
+
+                    modul.appendChild(modulmain);
+                    modulmain.appendChild(h1);
+                    modulmain.appendChild(h2);
+                    modulmain.appendChild(p);
+
+                }
+                getMovie();
+            })
+        })
+
     } else {
         const errorMessage = document.createElement('marquee')
         errorMessage.textContent = `oh no...our table...it's broken!`
@@ -59,19 +98,25 @@ let targetDiv = document.querySelector('.movie-card')
 // })
 
 function getID(e) {
-    let x = e.target.getAttribute("id");
-    console.log(x);
+
+
     var request = new XMLHttpRequest()
     request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
     request.onload = function () {
         // Begin accessing JSON data here
         var data = JSON.parse(this.response)
         console.log(data)
-        if (request.status >= 200 && request.status < 400) {
 
-        }
+        // document.getElementById("movie-app").innerHTML = ""
+        // const app = document.getElementById('movie-app')
+        // const container = document.createElement('div')
+        // container.setAttribute('class', 'container')
+        // app.appendChild(container)
+
+
     }
+
 }
 
-
-
+// let modul = document.getElementById("modul");
+// modul.style.display = 'block';
